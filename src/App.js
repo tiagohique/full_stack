f5ae8b87f299544b553412b64b94aa853d0c1078
Tiago  Silva <tiagosilva@Air-de-Tiago.lan>
2023-12-22 22:42:26 -0300

    refatorando

--- src/App.js
+++ src/App.js
@@ -1,34 +1,12 @@
 import './App.css';
-import Logo from './componentes/logo';
-import perfil from './imagens/perfil.svg'
-import sacola from './imagens/sacola.svg'
-
-const textoOpcap = ['CATEGORIAS', 'MINHA ESTANTE', 'FAVORITOS'];
-const icones = [perfil, sacola]
+import Header from './componentes/Header'
 
 function App() {
   return (
     <div className='App'>
-      <header className='App-header'>
-        <Logo />
-        <ul className='opcoes'>
-          {textoOpcap.map((texto) => (
-            <li className='opcao' key={texto}>
-              <p>{texto}</p>
-            </li>
-          ))}
-        </ul>
-
-
-        <ul className='icones'>
-          {icones.map((icone) => (
-            <li><img src={icone} className='icone'></img></li>
-          ))}
-        </ul>
-
-      </header>
+      <Header />
     </div>
   );
 }
 
-export default App;
+export default App
--- src/componentes/logo/index.js
+++ src/componentes/logo/index.js
@@ -1,14 +1,17 @@
-import logo from '../../imagens/logo.svg'
 import './estilo.css'
-
+import logo from '../../imagens/logo.svg'
 
 function Logo() {
     return (
         <div className='logo'>
-            <img src={logo} alt='logo' className='logo-img'></img>
-            <p><strong>Tiago </strong>Books</p>
-        </div>
+            <img
+                src={logo}
+                alt='logo' 
+                className='logo-img'
+            ></img>
+            <p><strong>Tiago</strong>Books</p>
+       </div>
     )
 }
 
-export default Logo;
+export default Logo
