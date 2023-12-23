624d6f2f8b277fcf474c77e14f73b72aaf864ad0
Tiago  Silva <tiagosilva@Air-de-Tiago.lan>
2023-12-22 22:01:34 -0300

    refatorando

--- src/App.js
+++ src/App.js
@@ -1,12 +1,30 @@
-import './App.css'
-import Logo from './componentes/logo'
+import './App.css';
+import Logo from './componentes/logo';
+import perfil from './imagens/perfil.svg'
+import sacola from './imagens/sacola.svg'
+
+const textoOpcap = ['CATEGORIAS', 'MINHA ESTANTE', 'FAVORITOS'];
+const icones = [perfil, sacola]
 
 function App() {
   return (
     <div className='App'>
       <header className='App-header'>
-        
-       <Logo />
+        <Logo />
+        <ul className='opcoes'>
+          {textoOpcap.map((texto) => (
+            <li className='opcao' key={texto}>
+              <p>{texto}</p>
+            </li>
+          ))}
+        </ul>
+
+
+        <ul className='icones'>
+          {icones.map((icone) => (
+            <li><img src={icone} className='icone'></img></li>
+          ))}
+        </ul>
 
       </header>
     </div>
--- src/componentes/logo/index.js
+++ src/componentes/logo/index.js
@@ -1,9 +1,11 @@
 import logo from '../../imagens/logo.svg'
+import './estilo.css'
+
 
 function Logo() {
     return (
         <div className='logo'>
-            <img src={logo} alt='logo'></img>
+            <img src={logo} alt='logo' className='logo-img'></img>
             <p><strong>Tiago </strong>Books</p>
         </div>
     )
