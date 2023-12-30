c4d382f5ee22fe681b00b35b8f98dee409ae2017
Tiago  Silva <tiagosilva@Air-de-Tiago.lan>
2023-12-30 16:09:55 -0300

    Tiago

--- src/componentes/pesquisa/index.js
+++ src/componentes/pesquisa/index.js
@@ -1,5 +1,7 @@
 import Input from '../Input'
 import Styled from 'styled-components'
+import { useState } from 'react'
+import { livros } from './dadosPesquisa'
 
 const PesquisaContainer = Styled.section`
         background-image: linear-gradient(90deg, #002F52 35%, #326589 165%);
@@ -9,7 +11,7 @@ const PesquisaContainer = Styled.section`
         height: 270px;
         width: 100%;
 `
-const Titulo =  Styled.h1`
+const Titulo = Styled.h1`
         color: #FFF;
         font-size: 36px;
         text-align: center;
@@ -21,14 +23,31 @@ const SubTitulo = Styled.h3`
         margin-bottom: 40px;
 `
 
+
 function Pesquisa() {
+    const [livrosPesquisados, setLivrosPesquisados] = useState([])
+
     return (
         <PesquisaContainer>
             <Titulo>Já sabe por onde começar</Titulo>
             <SubTitulo>encontre seu livre na nossa estante</SubTitulo>
             <Input
                 placeholder="Escreva sua próxima leitura"
+                onBlur={evento => {
+                    const textoDigitado = evento.target.value
+                    const resultadoPesquisa = livros.filter(livro => livro.nome.includes(textoDigitado))
+                    setLivrosPesquisados(resultadoPesquisa)
+                }}
             />
+
+
+            {livrosPesquisados.map(livro => (
+                <div>
+                    <p>{livro.nome}</p>
+                    <img src={livro.src} />
+                </div>
+            ))}
+
         </PesquisaContainer>
     )
 }
