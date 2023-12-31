5c33977a12102eb40a66838630e53ca791f50c29
Tiago  Silva <tiagosilva@Air-de-Tiago.lan>
2023-12-31 15:47:59 -0300

    Ultimo commit do ano 2023

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import Header from './componentes/Header'
 import styled from 'styled-components'
 import Pesquisa from './componentes/pesquisa'
+import UltimosLancamentos from './componentes/UltimosLancamentos'
 
 const AppContainer = styled.div`
     width: 100vw;
     height: 100vh;
-    background-image: linear-gradient(90deg,#002F52 35%,#326589 165%);
+    //background-image: linear-gradient(90deg,#002F52 35%,#326589 165%);
 `
 
 function App() {
@@ -13,7 +14,9 @@ function App() {
     <AppContainer>
        <Header />
        <Pesquisa />
+       <UltimosLancamentos />
     </AppContainer>
+       
   );
 }
 
